e4b4375d4d765e66bc456b9e96ca547c7e9440e1
Администратор <slegionv@yandex.ru>
2020-04-28 11:32:21 +0700

    added search slider

--- main.py
+++ main.py
@@ -251,6 +251,6 @@ def register():
 
 
 if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run()
